01f407a96495b89729c75247b848198d54b989c4
bfuzzy1 <54591490+bfuzzy1@users.noreply.github.com>
2023-11-25 17:20:47 -0500

    Update generate.go

--- AUTOMATE/generate.go
+++ AUTOMATE/generate.go
@@ -7,6 +7,7 @@ import (
 
 	"fmt"
 
+	"github.com/google/uuid"
 	"gopkg.in/yaml.v2"
 )
 
@@ -74,58 +75,71 @@ CREATED: {{.Created}}
 */
 package main
 
+// Import necessary packages based on configuration.
 {{- if .Imports}}
 import (
 {{- range .Imports}}
     {{- if .Alias}}
-        {{.Alias}} "{{.Path}}"
+    {{.Alias}} "{{.Path}}"
     {{- else}}
-        "{{.Path}}"
+    "{{.Path}}"
     {{- end}}
 {{- end}}
 )
 {{- end}}
 
+// Embedded files content.
 {{- range .EmbeddedFiles}}
 {{ .Content }}
-{{- end }}
+{{- end}}
 
 func test() {
-    // Endpoint calls
     {{- range $.EndpointCalls}}
-    {{- if eq .Function "Say"}}
+        {{- if eq .Function "Say"}}
+        // Say function logic for Endpoint.
         Endpoint.Say({{index .Arguments 0 | printf "%q"}})
-        
-    {{- else if eq .Function "Shell"}}
-        {{- range $cmdSet := .ShellCommands}}
-        command := []string{ {{- range $i, $cmd := $cmdSet}}{{if $i}}, {{end}}{{printf "%q" $cmd}}{{- end}} }
-        Endpoint.Shell(command)
-        
         {{- end}}
-    {{- else if eq .Function "Find"}}
+		
+        {{- if eq .Function "Shell"}}
+        // Shell function logic for Endpoint.
+            {{- range $cmdSet := .ShellCommands}}
+            command := []string{ {{- range $i, $cmd := $cmdSet}}{{if $i}}, {{end}}{{printf "%q" $cmd}}{{- end}} }
+            Endpoint.Shell(command)
+            {{- end}}
+        {{- end}}
+    
+        {{- if eq .Function "Find"}}
+        // Find function logic for Endpoint.
         Endpoint.Say("Starting scan for files")
         fileType := {{index .Arguments 0 | printf "%q"}}
         files := Endpoint.Find(fileType)
         if len(files) == 0 {
             Endpoint.Stop(104)
         }
-        
-    {{- else if eq .Function "Read"}}
+        {{- end}}
+    
+        {{- if eq .Function "Read"}}
+        // Read function logic for Endpoint.
         Endpoint.Say("Reading file")
         file := {{index .Arguments 0 | printf "%q"}}
-        contents := Endpoint.Read(file) // use contents for something later
-        
-    {{- else if eq .Function "Write"}}
+        contents := Endpoint.Read(file)
+        {{- end}}
+    
+        {{- if eq .Function "Write"}}
+        // Write function logic for Endpoint.
         Endpoint.Say("Writing file")
         Endpoint.Write({{index .Arguments 0 | printf "%q"}}, []byte({{index .Arguments 1 | printf "%q"}}))
-        
-    {{- else if eq .Function "Exists"}}
+        {{- end}}
+    
+        {{- if eq .Function "Exists"}}
+        // Exists function logic for Endpoint.
         Endpoint.Say("Checking if file exists")
         exists := Endpoint.Exists({{index .Arguments 0 | printf "%q"}})
-        
-    {{- else if eq .Function "Quarantined"}}
+        {{- end}}
+    
+        {{- if eq .Function "Quarantined"}}
+        // Quarantined function logic for Endpoint.
         Endpoint.Say("Extracting file for quarantine test")
-        Endpoint.Say("Pausing for 3 seconds to gauge defensive reaction")
         filename := {{index .Arguments 0 | printf "%q"}}
         if Endpoint.Quarantined(filename, malicious) {
             Endpoint.Say("Malicious file was caught!")
@@ -133,16 +147,13 @@ func test() {
         } else {
             Endpoint.Say("Malicious file was not caught")
         }
+        {{- end}}
     {{- end}}
-    
-    {{- if ne .Function "Clean"}}
-    
-    {{- end}}
-    
-{{- end}}
 
-{{- range $.NetworkCalls}}
-    {{- if eq .Function "GET"}}
+    // Handling Network calls.
+    {{- range $.NetworkCalls}}
+        {{- if eq .Function "GET"}}
+        // GET function logic for Network.
         Endpoint.Say("Executing GET Request")
         requestOptions := Network.RequestParameters{
             Headers: map[string][]string{"Content-Type": {"application/json"}},
@@ -161,35 +172,39 @@ func test() {
             Endpoint.Say("GET Response: " + string(response.Body))
         }
         
-	{{- else if eq .Function "POST"}}
-		Endpoint.Say("Executing POST Request")
-		requestOptions := Network.RequestParameters{
-			Headers: map[string][]string{"Content-Type": {"application/json"}},
-			Body: []byte("{{.Body}}"),
-		}
-		requester = Network.NewHTTPRequest("{{.URL}}", nil)
-		response, err = requester.POST(requestOptions)
-		if err != nil {
-			Endpoint.Say("POST Error: " + err.Error())
-		} else {
-			Endpoint.Say("POST Response: " + string(response.Body))
-		}
-	
-        
-    {{- else if eq .Function "TCP"}}
+        {{- else if eq .Function "POST"}}
+        // POST function logic for Network.
+        Endpoint.Say("Executing POST Request")
+        requestOptions := Network.RequestParameters{
+            Headers: map[string][]string{"Content-Type": {"application/json"}},
+            Body: []byte("{{.Body}}"),
+        }
+        requester := Network.NewHTTPRequest("{{.URL}}", nil)
+        response, err := requester.POST(requestOptions)
+        if err != nil {
+            Endpoint.Say("POST Error: " + err.Error())
+        } else {
+            Endpoint.Say("POST Response: " + string(response.Body))
+        }
+    
+        {{- else if eq .Function "TCP"}}
+        // TCP function logic for Network.
         Endpoint.Say("Executing TCP connection")
         Network.TCP("{{.Host}}", "{{.Port}}", []byte("{{.Message}}"))
         
-    {{- else if eq .Function "UDP"}}
+        {{- else if eq .Function "UDP"}}
+        // UDP function logic for Network.
         Endpoint.Say("Executing UDP connection")
         Network.UDP("{{.Host}}", "{{.Port}}", []byte("{{.Message}}"))
         
-    {{- else if eq .Function "ScanPort"}}
+        {{- else if eq .Function "ScanPort"}}
+        // ScanPort function logic for Network.
         Endpoint.Say("Executing Port Scan")
         isOpen := Network.ScanPort("{{.Protocol}}", "{{.Hostname}}", {{.Port}})
         Endpoint.Say(fmt.Sprintf("ScanPort: Port %d open: %v", {{.Port}}, isOpen))
         
-    {{- else if eq .Function "MultiplePortScan"}}
+        {{- else if eq .Function "MultiplePortScan"}}
+        // MultiplePortScan function logic for Network.
         Endpoint.Say("Executing Multi Port Scan")
         fmt.Println("Ports:", []int{ {{- range $i, $port := .Ports}}{{if $i}}, {{end}}{{ $port }}{{- end}} })
         for _, port := range []int{ {{- range $i, $port := .Ports}}{{if $i}}, {{end}}{{ $port }}{{- end}} } {
@@ -200,25 +215,23 @@ func test() {
                 fmt.Printf("Port %d is closed!\n", port)
             }
         }
+        {{- end}}
     {{- end}}
-    
-    {{- if ne .Function "Clean"}}
-    
-    {{- end}}
-    
-{{- end}}
-
-
 }
 
+// Function for cleanup operations.
 func clean() {
+    // Clean logic here.
     Endpoint.Say("Cleaning up")
 }
 
+// Main function to start the application.
 func main() {
+    // Starting the application with necessary endpoint calls.
     {{- range .EndpointCalls}}
         {{- if eq .Function "Start"}}
-            Endpoint.Start(test, clean)
+        // Starting the test and clean functions.
+        Endpoint.Start(test, clean)
         {{- end}}
     {{- end}}
 }
@@ -235,6 +248,16 @@ func main() {
 	err = yaml.Unmarshal(yamlFile, &conf)
 	if err != nil {
 		panic(err)
+	}
+
+	// Generate UUID for ID if not provided
+	if conf.ID == "" {
+		conf.ID = uuid.New().String()
+	}
+
+	// Generate timestamp for Created if not provided
+	if conf.Created == "" {
+		conf.Created = time.Now().Format("2006-01-02 15:04:05")
 	}
 
 	// Create generated_code directory if it doesn't exist
